84bace3c008ecc17076fabb897a01b56fd922caf
liuxiaole <liuxiaole@kingsoft.com>
2023-03-06 11:20:07 +0800

    不分逻辑调整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,11 +18,19 @@ import { SettingContext } from "../utils/settingContext";
 import { getApiKey } from "../utils/localStorage";
 
 export default function () {
-    const { reqParams, setReqParams } = useContext(SettingContext);
+    return (
+        <Grid templateColumns="repeat(4, 1fr)" gap={10} height="80px">
+            <GridItem colSpan={3}>
+                <FormApiKey />
+            </GridItem>
+            <GridItem colSpan={1}>
+                <FormTemperature />
+            </GridItem>
+        </Grid>
+    );
+}
 
-    const [temperature, setTemperature] = useState(6);
-    const [showTooltip, setShowTooltip] = useState(false);
-
+function FormApiKey(): JSX.Element {
     const [secretValue, setSecretValue] = useState(getSecretValue());
     const { setApiKey } = useContext(SettingContext);
 
@@ -45,64 +53,67 @@ export default function () {
     }
 
     return (
-        <Grid templateColumns="repeat(4, 1fr)" gap={10}>
-            <GridItem height="80px" colSpan={3}>
-                <FormControl>
-                    <FormLabel color="teal">当前秘钥</FormLabel>
-                    <Editable
-                        style={{
-                            textAlign: "left",
-                        }}
-                        placeholder="请输入你的秘钥……"
-                        value={secretValue}
-                        onSubmit={onSubmit}
-                        onChange={(v) => {
-                            setSecretValue(v);
-                        }}
-                        onEdit={() => {
-                            setSecretValue(getApiKey());
-                        }}
-                    >
-                        <EditablePreview style={{ color: "gray" }} />
-                        <EditableInput />
-                    </Editable>
-                </FormControl>
-            </GridItem>
-            <GridItem height="80px" colSpan={1}>
-                <FormControl>
-                    <FormLabel color="teal">Temperature:</FormLabel>
-                    <Slider
-                        id="slider"
-                        defaultValue={6}
-                        min={0}
-                        max={20}
-                        colorScheme="teal"
-                        onChange={(v) => {
-                            setTemperature(v);
-                            setReqParams({ ...reqParams, temperature: v / 10 });
-                        }}
-                        onMouseEnter={() => setShowTooltip(true)}
-                        onMouseLeave={() => setShowTooltip(false)}
-                    >
-                        <SliderMark value={10} mt="1" ml="-1" fontSize="sm">
-                            1
-                        </SliderMark>
-                        <SliderTrack>
-                            <SliderFilledTrack />
-                        </SliderTrack>
-                        <Tooltip
-                            hasArrow
-                            bg="teal.500"
-                            color="white"
-                            placement="top"
-                            isOpen={showTooltip}
-                            label={temperature / 10}
-                        >
-                            <SliderThumb />
-                        </Tooltip>
-                    </Slider>
-                </FormControl>
-            </GridItem>
-        </Grid>
+        <FormControl>
+            <FormLabel color="teal">当前秘钥</FormLabel>
+            <Editable
+                style={{
+                    textAlign: "left",
+                }}
+                placeholder="请输入你的秘钥……"
+                value={secretValue}
+                onSubmit={onSubmit}
+                onChange={(v) => {
+                    setSecretValue(v);
+                }}
+                onEdit={() => {
+                    setSecretValue(getApiKey());
+                }}
+            >
+                <EditablePreview style={{ color: "gray" }} />
+                <EditableInput />
+            </Editable>
+        </FormControl>
+    );
+}
+
+function FormTemperature(): JSX.Element {
+    const { reqParams, setReqParams } = useContext(SettingContext);
+    const [temperature, setTemperature] = useState(6);
+    const [showTooltip, setShowTooltip] = useState(false);
+
+    return (
+        <FormControl>
+            <FormLabel color="teal">Temperature:</FormLabel>
+            <Slider
+                id="slider"
+                defaultValue={6}
+                min={0}
+                max={20}
+                colorScheme="teal"
+                onChange={(v) => {
+                    setTemperature(v);
+                    setReqParams({ ...reqParams, temperature: v / 10 });
+                }}
+                onMouseEnter={() => setShowTooltip(true)}
+                onMouseLeave={() => setShowTooltip(false)}
+            >
+                <SliderMark value={10} mt="1" ml="-1" fontSize="sm">
+                    1
+                </SliderMark>
+                <SliderTrack>
+                    <SliderFilledTrack />
+                </SliderTrack>
+                <Tooltip
+                    hasArrow
+                    bg="teal.500"
+                    color="white"
+                    placement="top"
+                    isOpen={showTooltip}
+                    label={temperature / 10}
+                >
+                    <SliderThumb />
+                </Tooltip>
+            </Slider>
+        </FormControl>
     );
 }
--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,3 +1,5 @@
+import { RequestParams } from "./settingContext";
+
 export function getApiKey() {
     const apiKey: string | undefined = localStorage.getItem("openai-key") as string;
     if (apiKey === "undefined" || apiKey === null) {
@@ -5,3 +7,34 @@ export function getApiKey() {
     }
     return apiKey;
 }
+
+/*
+export interface Config {
+    apiKey: string;
+    reqParams?: RequestParams;
+}
+
+export function getConfig() {
+    const config: string | undefined = localStorage.getItem("config") as string;
+    if (config === "undefined" || config === null) {
+        return {} as Config;
+    }
+    return JSON.parse(config) as Config;
+}
+
+export function updateApiKey(v: string) {
+    const config = getConfig();
+    config.apiKey = v;
+    updateConfig(config);
+}
+
+export function updateReqParams(v: RequestParams) {
+    const config = getConfig();
+    config.reqParams = v;
+    updateConfig(config);
+}
+
+function updateConfig(config: Config) {
+    localStorage.setItem("config", JSON.stringify(config));
+}
+*/
