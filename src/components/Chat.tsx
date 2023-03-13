013ecb38d6c5e093bdc6f740f61260c577999445
Scholer <scholer_l@live.com>
2023-03-13 14:19:18 +0800

    修改模型

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -70,7 +70,7 @@ export default function () {
         try {
             const completion = await openai.createChatCompletion(
                 {
-                    model: "gpt-3.5-turbo-0301",
+                    model: "gpt-3.5-turbo",
                     messages: messages,
                     temperature: reqParams.temperature,
                     // stream: true,
